f6269b6a2a7977c780bdb860799779459f6b9aa7
Seth Todd <seth.todd@outlook.com>
2020-04-04 01:27:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
     
     def kind
-      if self.s1 <= 0 || self.s2 <= 0 || self.s3 <= 0 || (self.s1 + self.s2 < s3) || (self.s1 + self.s3 < s2) || (self.s2 + self.s3 < s1)
+      if self.s1 <= 0 || self.s2 <= 0 || self.s3 <= 0 || (self.s1 + self.s2 <= s3) || (self.s1 + self.s3 <= s2) || (self.s2 + self.s3 <= s1)
         raise TriangleError
       elsif (self.s1 == self.s2) && (self.s1 == self.s3)
         :equilateral
