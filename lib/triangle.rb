f93848eca6d50ce98618317775a606addd15c752
Seth Todd <seth.todd@outlook.com>
2020-04-04 01:09:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require "pry"
 class Triangle
-  # write code here
+  
+  attr_accessor :s1, :s2, :s3
+  
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+    
+    def kind
+      if self.s1 <= 
+      if (self.s1 == self.s2) && (self.s1 == self.s3)
+        :equilateral
+      elsif (self.s1 == self.s2) || (self.s1 == self.s3) || (self.s2 == self.s3)
+        :isosceles
+      elsif (self.s1 != self.s2) && (self.s1 != s3) && (self.s2 != s3)
+        :scalene
+      else
+        puts "Not a Triangle"
+      end
+  end
+  
+  class TriangleError < StandardError
+  end
+
+# binding.pry
+
 end
+
+
+
+
